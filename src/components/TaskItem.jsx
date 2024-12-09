ce7580e579bce871e1f92f920196817b25d36614
wicknesh <102829841+wicknesh@users.noreply.github.com>
2024-12-09 19:07:28 +0530

    Change styling input field and button

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,7 +1,7 @@
 import { CheckIcon, PencilIcon, TrashIcon } from "@heroicons/react/24/solid"
 import { useState } from "react"
 
-const TaskItem = ({ task, deleteTask, toggleTask }) => {
+const TaskItem = ({ task, deleteTask, toggleTask, editMode }) => {
 
     const [ isChecked, setIsChecked] = useState(task.checked);
 
@@ -11,7 +11,7 @@ const TaskItem = ({ task, deleteTask, toggleTask }) => {
     }
 
   return (
-    <li className="flex items-center justify-between gap-2.5">
+    <li className="flex items-center justify-between gap-2.5 border-2 border-solid border-[#433D8B] rounded-lg p-2.5 w-[28rem]">
         <div className="flex items-center gap-2">
             <input
                 type="checkbox"
@@ -19,7 +19,7 @@ const TaskItem = ({ task, deleteTask, toggleTask }) => {
                 name={task.name}
                 checked={isChecked}
                 onChange={handleCheckboxChange}
-                className="peer"
+                className="appearance-none peer w-5 h-5 cursor-pointer bg-gray-100 border-[#C8ACD6] border-2 rounded focus:ring-blue-500  checked:bg-[#C8ACD6]"
             />
             <label
                 htmlFor={task.id}
@@ -28,10 +28,10 @@ const TaskItem = ({ task, deleteTask, toggleTask }) => {
                 {task.name}
             </label>
         </div>
-        <div>
+        <div className="flex items-center gap-2">
             <button 
                 className="bg-[#433D8B] p-2 rounded"
-                // onClick={}
+                onClick={() => editMode(task)}
             >
                 <PencilIcon className='size-6' stroke='#C8ACD6' fill='#C8ACD6'/>
             </button>
