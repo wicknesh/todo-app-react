bca5f6c087e094c7f34daf064319171ebff18dc3
wicknesh <102829841+wicknesh@users.noreply.github.com>
2024-12-09 19:06:44 +0530

    Add important task button

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,7 +1,13 @@
-import { PlusIcon } from '@heroicons/react/24/solid'
+import { ExclamationTriangleIcon, PlusIcon } from '@heroicons/react/24/solid'
 import { useState } from 'react'
 
 const TaskForm = ({ addTask }) => {
+
+    const [ checked, setChecked ] = useState(false);
+    const toggleCheckbox = () => {
+      // console.log(!checked);
+      setChecked(!checked);
+    }
 
     const [task, setTask] = useState("");
     const handleFormSubmit = (e) => {
@@ -9,17 +15,19 @@ const TaskForm = ({ addTask }) => {
         addTask({
           id: Date.now(),
           name: task,
-          checked: false
+          checked: false,
+          important: checked
         });
         setTask("");
+        setChecked(false);
       }
   return (
     <form  onSubmit={handleFormSubmit}>
-        <div className='flex mt-10'>
+        <div className='flex mt-10 p-10 w-auto'>
           <input
             type="text"
             id="task"
-            className="mr-5 rounded-lg border-[#C8ACD6] border-solid border-[0.2rem] bg-slate-50 p-1 focus:outline-none focus:border-[#b069d3]"
+            className="mr-2 rounded-lg w-96 border-[#C8ACD6] border-solid border-[0.2rem] bg-slate-50 p-1 focus:outline-none focus:border-[#b069d3]"
             value={task}
             onInput={(e) => setTask(e.target.value)}
             required
@@ -27,11 +35,29 @@ const TaskForm = ({ addTask }) => {
             maxLength={60}
             placeholder="Enter task"
           />
+          <div className='flex items-center mr-2'>
+            <input
+              type="checkbox"
+              checked={checked}
+              onChange={toggleCheckbox}
+              className='hidden'
+              id='icon-checkbox'
+            />
+            <label
+              htmlFor="icon-checkbox"
+              // onClick={toggleCheckbox}
+              className={`cursor-pointer`}
+            >
+              {/* <ExclamationTriangleIcon className={`h-6 w-6 transition-colors ${checked? 'text-yellow-500' : 'text-gray-400'}`} /> */}
+              <ExclamationTriangleIcon className={`h-12 w-12 ${checked ? 'text-yellow-400' : 'text-gray-500'}`} />
+            </label>
+          </div>
+
           <button
             className="flex bg-[#433D8B] p-2 rounded"
             type="submit"
             >
-              <PlusIcon className='size-6' stroke='#C8ACD6' fill='#C8ACD6' />
+              <PlusIcon className='size-8' stroke='#C8ACD6' fill='#C8ACD6' />
           </button>
         </div>
       </form>
