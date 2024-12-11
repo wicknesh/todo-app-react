ca631afa1819d1c1f7b56d41bb3072e44c992e33
wicknesh <102829841+wicknesh@users.noreply.github.com>
2024-12-11 19:14:32 +0530

    Add media query

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,7 +2,7 @@ import TaskItem from "./TaskItem"
 
 const TaskList = ({ tasks, deleteTask, toggleTask, editMode }) => {
   return (
-    <ul className="grid list-none gap-6 text-center w-auto">
+    <ul className="grid list-none gap-6 text-center w-[25rem] xsm:w-[40rem] p-10">
         {tasks.sort((a, b) => b.important - a.important || b.id - a.id).map(task => (
             <TaskItem 
                 key={task.id}
