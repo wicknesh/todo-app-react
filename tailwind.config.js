e2a8a594d3ba3e9bb898107026179857bad52955
wicknesh <102829841+wicknesh@users.noreply.github.com>
2024-12-11 19:10:37 +0530

    Adjust screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
       screens: {
         'sm': '359px',
         'xs': '390px',
-        'xsm': '393px',
+        'xsm': '394px',
         'md': '768px',
         'lg': '1366px',
         'xl': '1536px'
