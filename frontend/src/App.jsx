3c769fef5493784e0f61631aaedeafeaa58e12dd
Vignesh J S <js.vignesh98@gmail.com>
2024-12-27 12:45:42 +0530

    Add API connections, add context provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,72 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 import EditForm from './components/EditForm';
-// import useLocalStorage from './hooks/useLocalStorage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Signup from './components/Signup';
+import NavBar from './components/NavBar';
+import { UserContext, UserProvider } from './utils/UserProvider.jsx';
+import axios from 'axios';
 
 function App() {
 
-  // const [tasks, setTasks] = useLocalStorage('todo.tasks', []);
   const [tasks, setTasks] = useState('');
   const [previousFocusEl, setPreviousFocusEl] = useState(null);
   const [editedTask, setEditedTask] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
+  const { user } = useContext(UserContext);
 
   const addTask = (task) => {
     setTasks(prevState => [...prevState, task]);
   }
 
-  const deleteTask = (id) => {
+  const clearTasks = () => {
+    setTasks([]);
+  }
+
+  const deleteTask = async (id) => {
     setTasks(prevState => prevState.filter(t => t.id !== id))
-  }
+    try {
+      const response = await axios.delete(`http://localhost:4500/tasks/delete-task/`, {
+        data: {uid: user?.uid, tid: id},
+    })
+    console.log(`Task deleted successfully`, response.data);
+    } catch (error) {
+      console.error(`Error deleting task:${error}`);
+    }
+ }
 
-  const toggleTask = (id) => {
+  const toggleTask = async (id) => {
     setTasks(prevState => prevState.map(t => (t.id === id ? { ...t, checked: !t.checked } : t )));
+    try {
+      const response = await axios.put(`http://localhost:4500/tasks/task-status/`, {
+        uid: user?.uid,
+        tid: id,
+      });
+      console.log(response.data.message);
+    } catch (error) {
+      console.error(`Error updating task: ${error}`);
+    }
   }
 
-  const updateTask = (task) => {
-    setTasks(prevState => prevState.map(t => (t.id === task.id ? { ...t, name: task.name, description: task.description, important: task.important } : t )));
-    closeEditMode();
+  const updateTask = async (task) => {
+    try {
+      const response = await axios.put(`http://localhost:4500/tasks/edit-task`, {
+        uid: user?.uid,
+        tid: task.id,
+        tname: task.name,
+        tdesc: task.description,
+        timportant: task.important
+      });
+
+      console.log(response.data.message);
+
+      setTasks(prevState => prevState.map(t => (t.id === task.id ? { ...t, name: task.name, description: task.description, important: task.important } : t )));
+      closeEditMode();
+    } catch (error) {
+      console.error(`Error updating task:${error}`);
+    }
   }
 
   const closeEditMode = () => {
@@ -42,31 +82,39 @@ function App() {
 
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <header>
-        <img className="w-60 h-auto mx-auto mt-24" src="../public/images/logo-white.png" alt="ToDoList Logo" />
-      </header>
-      {
-        isEditing && (
-          <EditForm
-            editedTask={editedTask}
-            updateTask={updateTask}
-            closeEditMode={closeEditMode}
-          />
-        )
-      }
-      <TaskForm
-        addTask={addTask}
-      />
-      { tasks && (
-        <TaskList
-          tasks={tasks}
-          deleteTask={deleteTask}
-          toggleTask={toggleTask}
-          editMode={editMode}
-        />
-      )}
-    </div>
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+          <Route path='/task' element={
+            <div className="flex flex-col items-center justify-center">
+              <NavBar clearTasks={clearTasks}/>
+              {
+                isEditing && (
+                  <EditForm
+                    editedTask={editedTask}
+                    updateTask={updateTask}
+                    closeEditMode={closeEditMode}
+                  />
+                )
+              }
+              <TaskForm
+                addTask={addTask}
+              />
+              { tasks && (
+                <TaskList
+                  tasks={tasks}
+                  deleteTask={deleteTask}
+                  toggleTask={toggleTask}
+                  editMode={editMode}
+                />
+              )}
+            </div>
+          } />
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
   )
 }
 
